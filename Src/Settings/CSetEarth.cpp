7db4850bb44cfe503fb737760d45d32b923bb0f9
fll0357 <2256263550@qq.com>
2022-10-17 18:15:35 +0800

    添加两个文件

---
+++ Src/Settings/CSetEarth.cpp
@@ -0,0 +1,43 @@
+#include "CSetEarth.h"
+
+CSetEarth::CSetEarth(QObject *parent)
+    : QObject{parent}
+{
+
+}
+
+void CSetEarth::setEarthName(const QString &sEarthName)
+{
+    if(sEarthName!=m_sEarthName)
+    {
+        m_sEarthName=sEarthName;
+        emit earthNameChanged(m_sEarthName);
+    }
+}
+
+void CSetEarth::setEarthPath(const QString &sEarthPath)
+{
+    if(sEarthPath!=m_sEarthPath)
+    {
+        m_sEarthPath=sEarthPath;
+        emit earthNameChanged(m_sEarthName);
+    }
+}
+
+void CSetEarth::setEarthLat(int nlat)
+{
+    if(nlat!=m_nLat)
+    {
+        m_nLat=nlat;
+        emit latChanged(m_nLat);
+    }
+}
+
+void CSetEarth::setEarthLon(int nlon)
+{
+    if(nlon!=m_nLon)
+    {
+        m_nLon=nlon;
+        emit lonChanged(m_nLon);
+    }
+}
